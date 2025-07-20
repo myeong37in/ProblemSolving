5f8d4519c439445fcd1e17e8528561fd4b8f09f9
Pindy-o-11 <87811760+myeong37in@users.noreply.github.com>
2025-07-20 23:19:04 +0900

    문제 추가

---
+++ BOJ/Rust/acmicpc1000.rs
@@ -0,0 +1,12 @@
+use std::io;
+
+fn main() {
+    let mut input_num = String::new();
+
+    io::stdin().read_line(&mut input_num).expect("input_num reading error");
+    
+    let numbers: Vec<i32> = input_num.split_whitespace().map(|s| s.parse().unwrap()).collect();
+    let sum: i32 = numbers[0] + numbers[1];
+
+    println!("{}", sum);
+}

8d217b86d5195f5e019dbcce71ae65b6065c4d48
Pindy-o-11 <87811760+myeong37in@users.noreply.github.com>
2025-10-22 13:20:32 +0900

    [Gold V] Title: Equal Sum Sets, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/9599. Equal Sum Sets/Equal Sum Sets.cc
@@ -0,0 +1,47 @@
+/*
+완전 탐색으로 10!이긴 함. DP가 될 것 같은데 방법이 떠오르지가 않네..
+*/
+
+#include <iostream>
+
+int n, k, s;
+
+int cnt = 0;
+static void backtrack(int prev, int sum, int depth) {
+    if (depth == k) {
+        if (sum == s) {
+            cnt++;
+        }
+        return;
+    }
+
+    for (int next = prev + 1; next <= n - k + 1 + depth; next++) {
+        backtrack(next, sum + next, depth + 1);
+    }
+}
+
+int main(int argc, char* argv[]){
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    while (std::cin >> n >> k >> s) {
+        cnt = 0;
+
+        if (n == 0) {
+            break;
+        }
+
+        if (n < k || (n * (n + 1) / 2) < s) { //수의 개수, 최소합 조건
+            std::cout << 0 << "\n";
+            continue;
+        }
+
+        for (int start = 1; start <= n - k + 1; start++) {
+            backtrack(start, start, 1);
+        }
+
+        std::cout << cnt << "\n";
+    }
+
+    return 0;
+}

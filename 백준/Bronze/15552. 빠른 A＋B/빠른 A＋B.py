25991d735aa2adb8bc75418c1dd89f6f86b16b15
Pindy-o-11 <87811760+myeong37in@users.noreply.github.com>
2025-10-28 17:49:33 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 1084 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.py
@@ -0,0 +1,10 @@
+import sys
+
+def main():
+    T = int(sys.stdin.readline().strip())
+    for _ in range(T):
+        A, B = map(int, sys.stdin.readline().split())
+        print(A + B)
+
+if __name__ == "__main__":
+    main()

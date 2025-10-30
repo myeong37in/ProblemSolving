362bd1d451da1396ba9b399a2489fffdafe92767
Pindy-o-11 <87811760+myeong37in@users.noreply.github.com>
2025-10-30 20:48:30 +0900

    [Bronze V] Title: A-B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.cc
@@ -0,0 +1,269 @@
+#include <iostream>
+
+int main() {
+    int A, B;
+    std::cin >> A >> B;
+    
+    if (A == 1) {
+        if (B == 1) {
+            std::cout << 0;
+        }
+        else if (B == 2) {
+            std::cout << -1;
+        }
+        else if (B == 3) {
+            std::cout << -2;
+        }
+        else if (B == 4) {
+            std::cout << -3;
+        }
+        else if (B == 5) {
+            std::cout << -4;
+        }
+        else if (B == 6) {
+            std::cout << -5;
+        }
+        else if (B == 7) {
+            std::cout << -6;
+        }
+        else if (B == 8) {
+            std::cout << -7;
+        }
+        else if (B == 9) {
+            std::cout << -8;
+        }
+    }
+    else if (A == 2) {
+        if (B == 1) {
+            std::cout << 1;
+        }
+        else if (B == 2) {
+            std::cout << 0;
+        }
+        else if (B == 3) {
+            std::cout << -1;
+        }
+        else if (B == 4) {
+            std::cout << -2;
+        }
+        else if (B == 5) {
+            std::cout << -3;
+        }
+        else if (B == 6) {
+            std::cout << -4;
+        }
+        else if (B == 7) {
+            std::cout << -5;
+        }
+        else if (B == 8) {
+            std::cout << -6;
+        }
+        else if (B == 9) {
+            std::cout << -7;
+        }
+    }
+    else if (A == 3) {
+        if (B == 1) {
+            std::cout << 2;
+        }
+        else if (B == 2) {
+            std::cout << 1;
+        }
+        else if (B == 3) {
+            std::cout << 0;
+        }
+        else if (B == 4) {
+            std::cout << -1;
+        }
+        else if (B == 5) {
+            std::cout << -2;
+        }
+        else if (B == 6) {
+            std::cout << -3;
+        }
+        else if (B == 7) {
+            std::cout << -4;
+        }
+        else if (B == 8) {
+            std::cout << -5;
+        }
+        else if (B == 9) {
+            std::cout << -6;
+        }
+    }
+    else if (A == 4) {
+        if (B == 1) {
+            std::cout << 3;
+        }
+        else if (B == 2) {
+            std::cout << 2;
+        }
+        else if (B == 3) {
+            std::cout << 1;
+        }
+        else if (B == 4) {
+            std::cout << 0;
+        }
+        else if (B == 5) {
+            std::cout << -1;
+        }
+        else if (B == 6) {
+            std::cout << -2;
+        }
+        else if (B == 7) {
+            std::cout << -3;
+        }
+        else if (B == 8) {
+            std::cout << -4;
+        }
+        else if (B == 9) {
+            std::cout << -5;
+        }
+    }
+    else if (A == 5) {
+        if (B == 1) {
+            std::cout << 4;
+        }
+        else if (B == 2) {
+            std::cout << 3;
+        }
+        else if (B == 3) {
+            std::cout << 2;
+        }
+        else if (B == 4) {
+            std::cout << 1;
+        }
+        else if (B == 5) {
+            std::cout << 0;
+        }
+        else if (B == 6) {
+            std::cout << -1;
+        }
+        else if (B == 7) {
+            std::cout << -2;
+        }
+        else if (B == 8) {
+            std::cout << -3;
+        }
+        else if (B == 9) {
+            std::cout << -4;
+        }
+    }
+    else if (A == 6) {
+        if (B == 1) {
+            std::cout << 5;
+        }
+        else if (B == 2) {
+            std::cout << 4;
+        }
+        else if (B == 3) {
+            std::cout << 3;
+        }
+        else if (B == 4) {
+            std::cout << 2;
+        }
+        else if (B == 5) {
+            std::cout << 1;
+        }
+        else if (B == 6) {
+            std::cout << 0;
+        }
+        else if (B == 7) {
+            std::cout << -1;
+        }
+        else if (B == 8) {
+            std::cout << -2;
+        }
+        else if (B == 9) {
+            std::cout << -3;
+        }
+    }
+    else if (A == 7) {
+        if (B == 1) {
+            std::cout << 6;
+        }
+        else if (B == 2) {
+            std::cout << 5;
+        }
+        else if (B == 3) {
+            std::cout << 4;
+        }
+        else if (B == 4) {
+            std::cout << 3;
+        }
+        else if (B == 5) {
+            std::cout << 2;
+        }
+        else if (B == 6) {
+            std::cout << 1;
+        }
+        else if (B == 7) {
+            std::cout << 0;
+        }
+        else if (B == 8) {
+            std::cout << -1;
+        }
+        else if (B == 9) {
+            std::cout << -2;
+        }
+    }
+    else if (A == 8) {
+        if (B == 1) {
+            std::cout << 7;
+        }
+        else if (B == 2) {
+            std::cout << 6;
+        }
+        else if (B == 3) {
+            std::cout << 5;
+        }
+        else if (B == 4) {
+            std::cout << 4;
+        }
+        else if (B == 5) {
+            std::cout << 3;
+        }
+        else if (B == 6) {
+            std::cout << 2;
+        }
+        else if (B == 7) {
+            std::cout << 1;
+        }
+        else if (B == 8) {
+            std::cout << 0;
+        }
+        else if (B == 9) {
+            std::cout << -1;
+        }
+    }
+    else if (A == 9) {
+        if (B == 1) {
+            std::cout << 8;
+        }
+        else if (B == 2) {
+            std::cout << 7;
+        }
+        else if (B == 3) {
+            std::cout << 6;
+        }
+        else if (B == 4) {
+            std::cout << 5;
+        }
+        else if (B == 5) {
+            std::cout << 4;
+        }
+        else if (B == 6) {
+            std::cout << 3;
+        }
+        else if (B == 7) {
+            std::cout << 2;
+        }
+        else if (B == 8) {
+            std::cout << 1;
+        }
+        else if (B == 9) {
+            std::cout << 0;
+        }
+    }
+    return 0;
+}

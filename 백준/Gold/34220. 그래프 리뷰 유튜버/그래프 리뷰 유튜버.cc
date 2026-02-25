61329d89a6d197ea561a6ef83d966e4573ce8dbe
Pindy-o-11 <87811760+myeong37in@users.noreply.github.com>
2026-02-25 17:48:55 +0900

    [Gold III] Title: 그래프 리뷰 유튜버, Time: 44 ms, Memory: 8448 KB -BaekjoonHub

---
+++ 백준/Gold/34220. 그래프 리뷰 유튜버/그래프 리뷰 유튜버.cc
@@ -0,0 +1,73 @@
+/*
+4개의 노드가 mesh형으로 6개의 간선으로 연결되어 있을 때 최소 4개 색을 쓰게 됨
+
+트리니까 일직선으로 연결된 4개의 정점을 고르면 항상 최소 3개의 간선으로 이 조건을 만족할 수 있음
+
+2개 이하의 간선은 불가능. 이 경우 사이클이 존재하므로
+
+1번부터 시작해서 4개 고릅시다.
+*/
+
+#include <iostream>
+#include <vector>
+
+std::vector<int> selected;
+std::vector<bool> visited;
+std::vector<std::vector<int>> graph; 
+
+static void dfs(int cur) {
+    if (selected.size() >= 4) {
+        return;
+    }
+    visited[cur] = true;
+    selected.emplace_back(cur);
+
+    for (int next : graph[cur]) {
+        if (!visited[next]) {
+            dfs(next);
+        }
+    }
+}
+
+int main(int argc, char* argv[]) {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    
+    int N;
+    std::cin >> N;
+
+    graph.resize(N + 1);
+    visited.resize(N + 1);
+    int u, v;
+    for (int i = 0; i < N - 1; i++) {
+        std::cin >> u >> v;
+        graph[u].emplace_back(v);
+        graph[v].emplace_back(u);
+    }
+
+    dfs(1);
+
+    std::vector<std::pair<int, int>> ans;
+    for (int i = 0; i < 3; i++) {
+        for (int j = i + 1; j < 4; j++) {
+            int u = selected[i], v = selected[j];
+            bool exist = false;
+            for (int n : graph[u]) {
+                if (n == v) {
+                    exist = true;
+                }
+            }
+
+            if (!exist) {
+                ans.emplace_back(u, v);
+            }
+        }
+    }
+
+    std::cout << ans.size() << "\n";
+    for (auto& [u, v] : ans) {
+        std::cout << u << " " << v << "\n";
+    }
+    
+    return 0;
+}

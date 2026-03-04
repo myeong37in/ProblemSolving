5de8599d4e804053fd91fea9f0c3e0f212cb5a6f
Pindy-o-11 <87811760+myeong37in@users.noreply.github.com>
2026-03-04 18:00:01 +0900

    [Gold V] Title: 시간 구간 다중 업데이트 다중 합, Time: 4 ms, Memory: 2704 KB, Score: 30 point -BaekjoonHub

---
+++ 백준/Gold/25827. 시간 구간 다중 업데이트 다중 합/시간 구간 다중 업데이트 다중 합.cc
@@ -0,0 +1,56 @@
+/*
+실시간 처리가 아님 
+
+시작에서 +1, 끝에서 -1, 누적 합
+*/
+
+#include <iostream>
+#include <vector>
+
+static inline int GetTimeSec(std::string timestamp) {
+    return ((timestamp[0] - '0') * 10 + (timestamp[1] - '0')) * 3600 + \
+           ((timestamp[3] - '0') * 10 + (timestamp[4] - '0')) * 60 + \
+           ((timestamp[6] - '0') * 10 + (timestamp[7] - '0'));
+}
+
+int main(int argc, char* argv[]) {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    
+
+    constexpr int cnt = 24 * 60 * 60;
+    std::vector<int> diffs(cnt), psum(cnt);
+
+    int n;
+    std::cin >> n;
+
+    int type;
+    bool type1_end = false;
+    std::string stime, etime;
+    for (int i = 0; i < n; i++) {
+        std::cin >> type >> stime >> etime;
+        
+        int stime_int = GetTimeSec(stime);
+        int etime_int = GetTimeSec(etime);
+        int diff = 0; // 현재 시각에서 변화량
+
+        if (type == 1) {
+            diffs[stime_int]++;
+            diffs[etime_int]--;
+        }
+        else {
+            // 1번 유형의 쿼리 끝. 계산
+            if (type1_end == false) {
+                for (int i = 0; i < cnt - 1; i++) {
+                    diff += diffs[i];
+                    psum[i + 1] = psum[i] + diff;
+                }
+                type1_end = true;
+            }
+
+            std::cout << psum[etime_int] - psum[stime_int] << "\n";
+        }
+    }
+    
+    return 0;
+}
